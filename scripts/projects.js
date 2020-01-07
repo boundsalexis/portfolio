dfc084f866f60494ef01611dd237cbc2082aed9f
Alexis <lxs.bnds@gmail.com>
2020-01-07 13:13:07 -0800

    increased mobile responsiveness

--- scripts/projects.js
+++ scripts/projects.js
@@ -57,3 +57,23 @@ home.addEventListener("click", function(){
   window.location.href = './index.html';
 
 })
+
+function mediaQuery(viewport) {
+  if (viewport.matches) { // If media query matches
+    projects.classList.remove("fa-2x");
+    contact.classList.remove("fa-2x");
+    github.classList.remove("fa-2x");
+    linkedin.classList.remove("fa-2x");
+    bio.classList.remove("fa-2x");
+  } else {
+    projects.classList.add("fa-2x");
+    contact.classList.add("fa-2x");
+    github.classList.add("fa-2x");
+    linkedin.classList.add("fa-2x");
+    bio.classList.add("fa-2x");
+  }
+}
+
+var viewport = window.matchMedia("(max-width: 600px)");
+mediaQuery(viewport); // Call listener function at run time
+viewport.addListener(mediaQuery);
